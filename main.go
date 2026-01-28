aed670a979ebff8874396f2fd779e77ef7a21b56
asatriana <asatriana@gmail.com>
2026-01-28 22:29:10 +0700

    CODE-03: In-Memory Storage

--- main.go
+++ main.go
@@ -15,3 +15,10 @@ type Produk struct {
 	Harga int    `json:"harga"`
 	Stok  int    `json:"stok"`
 }
+
+// In-memory storage (sementara, nanti ganti database)
+var produk = []Produk{
+	{ID: 1, Nama: "Indomie Godog", Harga: 3500, Stok: 10},
+	{ID: 2, Nama: "Vit 1000ml", Harga: 3000, Stok: 40},
+	{ID: 3, Nama: "kecap", Harga: 12000, Stok: 20},
+}
