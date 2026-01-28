e2cd6e49c47687452be7755ec6770e34689163a1
asatriana <asatriana@gmail.com>
2026-01-28 22:27:12 +0700

    CODE-02: Produk Struct

--- main.go
+++ main.go
@@ -7,3 +7,11 @@ import (
 	"strconv"
 	"strings"
 )
+
+// Produk represents a product in the cashier system
+type Produk struct {
+	ID    int    `json:"id"`
+	Nama  string `json:"nama"`
+	Harga int    `json:"harga"`
+	Stok  int    `json:"stok"`
+}
