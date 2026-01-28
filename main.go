1b76afaa776cec22a1385f35f39c759bc1c79b95
asatriana <asatriana@gmail.com>
2026-01-28 22:31:17 +0700

    CODE-04: Health Check Endpoint

--- main.go
+++ main.go
@@ -22,3 +22,12 @@ var produk = []Produk{
 	{ID: 2, Nama: "Vit 1000ml", Harga: 3000, Stok: 40},
 	{ID: 3, Nama: "kecap", Harga: 12000, Stok: 20},
 }
+
+// localhost:8080/health
+http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"status":  "OK",
+		"message": "API Running",
+	})
+})
